ca1b7d5ca7557375396bc23b6ae880003a089d8f
mundahl <jhmsoccer+github@gmail.com>
2024-04-26 22:57:34 -0500

    swap .ino out for .c and HAL
    Changed to a diff pin b/c copying this implementation. Build off the best!

---
+++ main.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stm32c0xx_hal.h>
+
+// ST Nucleo Green user LED (PA5)
+#define LED_PORT                GPIOA
+#define LED_PIN                 GPIO_PIN_5
+#define LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOA_CLK_ENABLE
+
+UART_HandleTypeDef huart2;
+
+void SystemClock_Config(void);
+static void MX_USART2_UART_Init(void);
+
+void osSystickHandler(void)
+{
+  // 1 Hz blinking:
+  if ((HAL_GetTick() % 500) == 0)
+  {
+    HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
+  }
+}
+
+void initGPIO()
+{
+  GPIO_InitTypeDef GPIO_Config;
+
+  GPIO_Config.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_Config.Pull = GPIO_NOPULL;
+  GPIO_Config.Speed = GPIO_SPEED_FREQ_HIGH;
+
+  GPIO_Config.Pin = LED_PIN;
+
+  LED_PORT_CLK_ENABLE();
+  HAL_GPIO_Init(LED_PORT, &GPIO_Config);
+
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+}
+
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+
+  initGPIO();
+  MX_USART2_UART_Init();
+
+  printf("Hello, %s!\n", "Wokwi");
+
+  HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
+
+  while (1);
+
+  return 0;
+}
+
+/**
+    @brief System Clock Configuration
+    @retval None
+*/
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+    in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                | RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+    @brief USART2 Initialization Function
+    @param None
+    @retval None
+*/
+static void MX_USART2_UART_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  // PA2     ------> USART2_TX
+  // PA3     ------> USART2_RX
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF1_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+
+void Error_Handler(void)
+{
+  /* User can add his own implementation to report the HAL error return state */
+}
+
+
+// The following makes printf() write to USART2:
+
+#define STDOUT_FILENO   1
+#define STDERR_FILENO   2
+
+int _write(int file, uint8_t *ptr, int len)
+{
+  switch (file)
+  {
+    case STDOUT_FILENO:
+      HAL_UART_Transmit(&huart2, ptr, len, HAL_MAX_DELAY);
+      break;
+
+    case STDERR_FILENO:
+      HAL_UART_Transmit(&huart2, ptr, len, HAL_MAX_DELAY);
+      break;
+
+    default:
+      return -1;
+  }
+
+  return len;
+}
